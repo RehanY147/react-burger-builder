12c5a06c0bb7bf967562790cf741d9b5b956afa8
Rehan Yousaf <rehanyousaf147@gmail.com>
2020-06-24 00:09:12 +0500

    Adding responsiveness and Navigation

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Auxiliary from '../../hoc/Auxiliary';
 import classes from './Layout.module.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import Sidebar from '../Navigation/Sidebar/Sidebar';
 
 const layout = (props) => {
     return (
         <Auxiliary>
             <Toolbar />
+            <Sidebar />
             <main className={classes.Content}>
                 {props.children}
             </main>
